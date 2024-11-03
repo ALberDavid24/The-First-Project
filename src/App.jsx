b045f6d02a24a8e7ce21d6db121a3f73e82bf8e5
Albert <alberprain@gmail.com>
2024-11-02 20:41:26 -0500

    first change about fetch in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, {useState, useEffect} from "react";
+import "./App.css";
+import Barra from "./components/Barra.Jsx";
+import Cards from "./components/Cards";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [characters, setCharacters] = useState([]);
+
+  const initialurl = "https://dragonball-api.com/api/characters/1";
+  
+  const fetchCharacters = (url) => {
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        if (data.results) {
+          setCharacters(data.results);
+        } else {
+          setCharacters(data); 
+        }
+      })
+      .catch(error => console.error("Error fetching characters:", error));
+  };
+  
+  useEffect(() => {
+    fetchCharacters(initialurl);
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Puto el que lo lea</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 2)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <h1>ESENCIA</h1>
+      <Barra />
+      <div className="container">
+        <Cards characters={characters} />
+      </div> 
+    </div>
+  );
 }
 
 export default App
